c78e44fed25374e33668c4d4d0bfd7275a074ce9
Shirley Tenelanda <shirleytenelanda622@gmail.com>
2026-06-09 21:38:58 -0500

    Add cerrar method to exit application

--- src/main/java/com/example/tarea_practica/CajeroController.java
+++ src/main/java/com/example/tarea_practica/CajeroController.java
@@ -1,4 +1,12 @@
 package com.example.tarea_practica;
 
+import javafx.application.Platform;
+import javafx.fxml.FXML;
+
 public class CajeroController {
+    @FXML
+    public void cerrar() {
+
+        Platform.exit();
+    }
 }
